f9d666e96c246b0fdeb761a96a18a36e17b734f7
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2022-01-13 23:49:14 +0000

    Added tests for blog

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            username='yoofi', email='test@gmail.com', password='secret')
+
+        self.post = Post.objects.create(
+            title='Test Post', body='Test body', author=self.user)
+
+    def test_string_representation(self):
+        post = Post(title="This is a test")
+        self.assertEqual(post.title, str(post))
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'Test Post')
+        self.assertEqual(f'{self.post.body}', 'Test body')
+        self.assertEqual(f'{self.post.author}', 'yoofi')
+
+    def test_post_list_view(self):
+        resp = self.client.get(reverse('blog:index'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'blog/index.html')
+
+    def test_post_detail_view(self):
+        resp = self.client.get('/blog/post/1/')
+        self.assertEqual(resp.status_code, 200)
+        no_resp = self.client.get('/blog/post/50/')
+        self.assertEqual(no_resp.status_code, 404)
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='index')
+    path('', BlogListView.as_view(), name='index'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='show')
 ]
